b84803139f15ae2908cf8c5e27394d71df46a7e3
Cation <J6597@tutanota.com>
2022-04-17 17:26:46 +0300

    fixed

--- BinarySearch/binarySearch.go
+++ BinarySearch/binarySearch.go
@@ -6,25 +6,25 @@ import (
 
 func main(){
     //the array has to be sorted if it is not already
-    arr := []int{0,3,22,133,242,344,545,666,667,775,876,984,1002,1003,2043};
-    result := binarySearch(arr, 0, len(arr), 775);
-    if result != -1{
+    arr := []int{0,3,22,133,242,344,545,666,667,775,876,984,1002,1003,2043}
+    result, ok := binarySearch(arr, 0, len(arr), 775)
+    if ok != false {
         fmt.Println(result)
     }
 }
 
-func binarySearch(arr []int, from, to, x int) int {
+func binarySearch(arr []int, from, to, x int) (int, bool) {
     for from <= to {
-        mid := from + (to - from) / 2;
+        mid := from + (to - from) / 2
         fmt.Println("mid: ",mid)
         if arr[mid] == x{
-            return mid;
+            return mid, true
         }else if arr[mid] < x {
-            from = mid + 1;
+            from = mid + 1
         }else {
-            to = mid - 1;
+            to = mid - 1
         }
     }
-    return -1;
+    return 0, false
 }
  
